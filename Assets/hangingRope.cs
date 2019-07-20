93de433fa8e0295759d39462b2bb06164b10d48b
Elliot Cuzzillo <elliotc@unity3d.com>
2019-07-20 16:26:24 -0700

    Making rope ropier.

--- Assets/hangingRope.cs
+++ Assets/hangingRope.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class hangingRope : MonoBehaviour
 {
+    public bool ropeTracking;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        var ropeLine = GetComponent<LineRenderer>();
+        ropeLine.SetWidth(.3f, .3f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (ropeTracking)
+        {
+            var ropeLine = GetComponent<LineRenderer>();
+            ropeLine.SetPosition(1, Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        Debug.Log("");
+        ropeTracking = !ropeTracking;
     }
 }
